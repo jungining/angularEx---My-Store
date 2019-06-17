c7f336ea00b84d1b07ef194c8db9234a1a5e12d3
jungining <jungin641@gmail.com>
2019-06-17 13:52:26 +0900

    Enable HttpClient for cart service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items = [];
+
+   constructor(
+    private http: HttpClient
+  ) {}
 
   addToCart(product) {
     this.items.push(product);
@@ -17,5 +24,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+    getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
   }
 }
